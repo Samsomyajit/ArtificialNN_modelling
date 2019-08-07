23ce27a0d64697e914827d84b1ffe048986c6e00
Somyajit Chakraborty Sam <41539319+Samsomyajit@users.noreply.github.com>
2019-08-07 19:51:50 +0530

    Add files via upload

---
+++ ann_k_fold_cross_val.py
@@ -0,0 +1,20 @@
+#code for K Fold Cross Validation
+
+# !!IMPORTANT!! - PERFORM DATA PREPROCESSING BEFORE EXECUTING THE STEPS BELOW
+# Note - You cannot save a model after K fold cross val as its purpose is for testing model not creating
+
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from keras.models import Sequential
+from keras.layers import Dense
+def build_classifier():
+    classifier = Sequential()
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier
+classifier = KerasClassifier(build_fn = build_classifier, batch_size = 10, epochs = 100)
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10, n_jobs = -1)
+mean = accuracies.mean()
+variance = accuracies.std()
